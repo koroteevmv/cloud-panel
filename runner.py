94f1c43e45ec938f8cc1df0c1b823ae4801e0f1c
Michail Koroteev <mvkoroteev@fa.ru>
2023-12-28 14:06:11 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -27,5 +27,4 @@ rescan_db()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False,
-            ssl_context=('/etc/ssl/koroteev.site.crt', '/etc/ssl/koroteev.site.key'))
+    app.run(host='0.0.0.0', debug=False)
